5157508c97fc75130a9ed893bbdc65de9c1ea750
iammar7 <maansaridot@gmail.com>
2020-12-10 00:07:48 +0500

    type chart added on region chart click

--- src/components/Charts/RegionChart.jsx
+++ src/components/Charts/RegionChart.jsx
@@ -1,70 +1,83 @@
-import React from 'react';
-import { Row, Typography, Badge } from 'antd';
+import React, { useRef, useEffect, useState } from 'react';
+import { Modal, Badge, Typography } from 'antd';
 import { Bar } from '@ant-design/charts';
 
+import TypeChart from './TypeChart';
+
 const RegionChart = ({ stats }) => {
-  const data = [
-    {
-      region: 'Southern',
-      status: 'Resolved',
-      count: 1,
-    },
-    {
-      region: 'CR-East',
-      status: 'Pending',
-      count: 1,
-    },
-    {
-      region: 'WR-South',
-      status: 'Pending',
-      count: 6,
-    },
-    {
-      region: 'Southern',
-      status: 'Pending',
-      count: 2,
-    },
-    {
-      region: 'ER-North',
-      status: 'Resolved',
-      count: 1,
-    },
-    {
-      region: 'WR-South',
-      status: 'Resolved',
-      count: 2,
-    },
-    {
-      region: 'CR-East',
-      status: 'Cancelled',
-      count: 1,
-    },
-    {
-      region: 'CR-East',
-      status: 'Resolved',
-      count: 2,
-    },
-    {
-      region: 'CR-South',
-      status: 'Resolved',
-      count: 2,
-    },
-    {
-      region: 'ER-South',
-      status: 'Resolved',
-      count: 2,
-    },
-    {
-      region: 'CR-North',
-      status: 'Resolved',
-      count: 3,
-    },
-    {
-      region: 'CR-South',
-      status: 'Pending',
-      count: 1,
-    },
-  ];
+  const [selectedData, setSelectedData] = useState(null);
+  const [displayModal, setDisplayModal] = useState(false);
+
+  const handleOk = () => {
+    setDisplayModal(false);
+  };
+
+  const handleCancel = () => {
+    setDisplayModal(false);
+  };
+
+  // const data = [
+  //   {
+  //     region: 'Southern',
+  //     status: 'Resolved',
+  //     count: 1,
+  //   },
+  //   {
+  //     region: 'CR-East',
+  //     status: 'Pending',
+  //     count: 1,
+  //   },
+  //   {
+  //     region: 'WR-South',
+  //     status: 'Pending',
+  //     count: 6,
+  //   },
+  //   {
+  //     region: 'Southern',
+  //     status: 'Pending',
+  //     count: 2,
+  //   },
+  //   {
+  //     region: 'ER-North',
+  //     status: 'Resolved',
+  //     count: 1,
+  //   },
+  //   {
+  //     region: 'WR-South',
+  //     status: 'Resolved',
+  //     count: 2,
+  //   },
+  //   {
+  //     region: 'CR-East',
+  //     status: 'Cancelled',
+  //     count: 1,
+  //   },
+  //   {
+  //     region: 'CR-East',
+  //     status: 'Resolved',
+  //     count: 2,
+  //   },
+  //   {
+  //     region: 'CR-South',
+  //     status: 'Resolved',
+  //     count: 2,
+  //   },
+  //   {
+  //     region: 'ER-South',
+  //     status: 'Resolved',
+  //     count: 2,
+  //   },
+  //   {
+  //     region: 'CR-North',
+  //     status: 'Resolved',
+  //     count: 3,
+  //   },
+  //   {
+  //     region: 'CR-South',
+  //     status: 'Pending',
+  //     count: 1,
+  //   },
+  // ];
 
   const config = {
     data: stats,
@@ -85,62 +98,98 @@ const RegionChart = ({ stats }) => {
       },
       style: { fill: '#fff' },
     },
-    tooltip: {
-      customContent: (title, values) => {
-        console.log(title, values);
-        return (
-          <div style={{ padding: '2px 6px' }}>
-            <Row style={{ margin: '8px 0px' }}>
-              <Typography.Text strong>{title}</Typography.Text>
-            </Row>
-            {values.map((item) => (
-              <div style={{ marginBottom: '12px' }} key={item.name}>
-                <Row>
-                  <Badge
-                    color={item.color}
-                    text={
-                      <Typography.Text>
-                        {item.name} <Typography.Text strong>{item.value}</Typography.Text>
-                      </Typography.Text>
-                    }
-                  />
-                </Row>
-                <Row style={{ margin: '4px 0px', paddingLeft: '12px' }}>
-                  <Typography.Text>
-                    Customer Experience {item.data['Customer Experience'] ?? 0}
-                  </Typography.Text>
-                </Row>
-                <Row style={{ marginBottom: '4px', paddingLeft: '12px' }}>
-                  <Typography.Text>Bay Violation {item.data['Bay Violation'] ?? 0}</Typography.Text>
-                </Row>
-                <Row style={{ marginBottom: '4px', paddingLeft: '12px' }}>
-                  <Typography.Text>Housekeeping {item.data['Housekeeping'] ?? 0}</Typography.Text>
-                </Row>
-                <Row style={{ marginBottom: '4px', paddingLeft: '12px' }}>
-                  <Typography.Text>
-                    Customer Mistreatment {item.data['Customer Mistreatment'] ?? 0}
-                  </Typography.Text>
-                </Row>
-                <Row style={{ marginBottom: '4px', paddingLeft: '12px' }}>
-                  <Typography.Text>Initiative {item.data['Initiative'] ?? 0}</Typography.Text>
-                </Row>
-                <Row style={{ marginBottom: '4px', paddingLeft: '12px' }}>
-                  <Typography.Text>Admin Issues {item.data['Admin Issues'] ?? 0}</Typography.Text>
-                </Row>
-                <Row style={{ marginBottom: '4px', paddingLeft: '12px' }}>
-                  <Typography.Text>Safety {item.data['Safety'] ?? 0}</Typography.Text>
-                </Row>
-                <Row style={{ marginBottom: '4px', paddingLeft: '12px' }}>
-                  <Typography.Text>Others {item.data['Others'] ?? 0}</Typography.Text>
-                </Row>
-              </div>
-            ))}
-          </div>
-        );
-      },
-    },
+    // tooltip: false,
+    // tooltip: {
+    //   customContent: (title, values) => {
+    //     console.log(title, values);
+    //     return (
+    //       <div style={{ padding: '2px 6px' }}>
+    //         <Row style={{ margin: '8px 0px' }}>
+    //           <Typography.Text strong>{title}</Typography.Text>
+    //         </Row>
+    //         <TypeChart />
+    //         {/* {values.map((item) => (
+    //           <div style={{ marginBottom: '12px' }} key={item.name}>
+    //             <Row>
+    //               <Badge
+    //                 color={item.color}
+    //                 text={
+    //                   <Typography.Text>
+    //                     {item.name} <Typography.Text strong>{item.value}</Typography.Text>
+    //                   </Typography.Text>
+    //                 }
+    //               />
+    //             </Row>
+    //             <Row style={{ margin: '4px 0px', paddingLeft: '12px' }}>
+    //               <Typography.Text>
+    //                 Customer Experience {item.data['Customer Experience'] ?? 0}
+    //               </Typography.Text>
+    //             </Row>
+    //             <Row style={{ marginBottom: '4px', paddingLeft: '12px' }}>
+    //               <Typography.Text>Bay Violation {item.data['Bay Violation'] ?? 0}</Typography.Text>
+    //             </Row>
+    //             <Row style={{ marginBottom: '4px', paddingLeft: '12px' }}>
+    //               <Typography.Text>Housekeeping {item.data['Housekeeping'] ?? 0}</Typography.Text>
+    //             </Row>
+    //             <Row style={{ marginBottom: '4px', paddingLeft: '12px' }}>
+    //               <Typography.Text>
+    //                 Customer Mistreatment {item.data['Customer Mistreatment'] ?? 0}
+    //               </Typography.Text>
+    //             </Row>
+    //             <Row style={{ marginBottom: '4px', paddingLeft: '12px' }}>
+    //               <Typography.Text>Initiative {item.data['Initiative'] ?? 0}</Typography.Text>
+    //             </Row>
+    //             <Row style={{ marginBottom: '4px', paddingLeft: '12px' }}>
+    //               <Typography.Text>Admin Issues {item.data['Admin Issues'] ?? 0}</Typography.Text>
+    //             </Row>
+    //             <Row style={{ marginBottom: '4px', paddingLeft: '12px' }}>
+    //               <Typography.Text>Safety {item.data['Safety'] ?? 0}</Typography.Text>
+    //             </Row>
+    //             <Row style={{ marginBottom: '4px', paddingLeft: '12px' }}>
+    //               <Typography.Text>Others {item.data['Others'] ?? 0}</Typography.Text>
+    //             </Row>
+    //           </div>
+    //         ))} */}
+    //       </div>
+    //     );
+    //   },
+    // },
   };
 
-  return <Bar {...config} />;
+  const ref = useRef();
+  useEffect(() => {
+    // element adds a click event, element refers to label|point, etc.
+    ref.current.on('element:click', (e) => {
+      setSelectedData(e.data ?? null);
+      setDisplayModal(true);
+    });
+  }, []);
+
+  return (
+    <>
+      <Bar {...config} chartRef={ref} />
+      <Modal
+        title={
+          <>
+            <Typography.Title level={4}>{selectedData?.data?.region}</Typography.Title>
+            <Badge
+              color={`${selectedData?.color}`}
+              text={`${selectedData?.data?.status} ${selectedData?.data?.count}`}
+            />
+          </>
+        }
+        visible={displayModal}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={null}
+        width={600}
+        bodyStyle={{
+          height: '300px',
+        }}
+      >
+        <TypeChart stats={selectedData} />
+      </Modal>
+    </>
+  );
 };
 export default RegionChart;
---
+++ src/components/Charts/TypeChart.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Pie } from '@ant-design/charts';
+
+const TypeChart = ({ stats }) => {
+  const data = [
+    {
+      type: 'Customer Experience',
+      count: stats.data['Customer Experience'] ?? null,
+    },
+    {
+      type: 'Bay Violation',
+      count: stats.data['Bay Violation'] ?? null,
+    },
+    {
+      type: 'Housekeeping',
+      count: stats.data['Housekeeping'] ?? null,
+    },
+    {
+      type: 'Customer Mistreatment',
+      count: stats.data['Customer Mistreatment'] ?? null,
+    },
+    {
+      type: 'Initiative',
+      count: stats.data['Initiative'] ?? null,
+    },
+    {
+      type: 'Admin Issues',
+      count: stats.data['Admin Issues'] ?? null,
+    },
+    {
+      type: 'Safety',
+      count: stats.data['Safety'] ?? null,
+    },
+    {
+      type: 'Others',
+      count: stats.data['Others'] ?? null,
+    },
+  ];
+
+  const config = {
+    height: 400,
+    width: 400,
+    data,
+    angleField: 'count',
+    colorField: 'type',
+    radius: 0.9,
+    label: {
+      type: 'inner',
+      offset: '-30%',
+      style: {
+        fontSize: 16,
+        textAlign: 'center',
+      },
+    },
+    legend: {
+      flipPage: false,
+      itemHeight: 20,
+    },
+    interactions: [{ type: 'element-active' }],
+  };
+
+  return <Pie {...config} />;
+};
+
+export default TypeChart;
